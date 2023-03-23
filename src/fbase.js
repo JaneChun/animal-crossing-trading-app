df03f88c436dc0d6dc7f65e416303dedfb5a6b95
JaneChun <janechun22@gmail.com>
2023-03-24 01:22:07 +0900

    router, tailwind, firebase initial setting

---
+++ src/fbase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getAnalytics } from 'firebase/analytics';
+
+// Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+	measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+export const firebase = initializeApp(firebaseConfig);
+export const auth = getAuth(firebase);
+export const db = getFirestore(firebase);
+export const storage = getStorage(firebase);
+export const analytics = getAnalytics(firebase);
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			backgroundImage: (theme) => ({
+				'basic-profile': "url('https://tiermaker.com/images/templates/4968001594567697.jpg')",
+			}),
+		},
+	},
+	plugins: [],
+};
