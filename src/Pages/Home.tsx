b7d62470c5d74fc0553c42d4a5ff6180454f4375
JaneChun <janechun22@gmail.com>
2023-03-24 19:16:46 +0900

    feat: read post

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Footer from '../Components/Footer';
+import Carousel from '../Components/Carousel';
+import { auth, db } from '../fbase';
+import { collection, getDocs, query, orderBy } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
+
+interface doc {
+	id: string;
+	title?: string;
+	body?: string;
+	creatorDisplayName?: string;
+	creatorId?: string;
+	createdAt?: number;
+}
 
 function Home() {
-	return <div>Home</div>;
+	const navigate = useNavigate();
+	const [data, setData] = useState<doc[]>([]);
+	const userInfo = auth.currentUser;
+
+	useEffect(() => {
+		console.log('useEffect 실행');
+		getData();
+	}, []);
+
+	const getData = async () => {
+		const q = query(collection(db, 'Boards'), orderBy('createdAt', 'desc'));
+		const querySnapshot = await getDocs(q);
+		querySnapshot.forEach((doc) => {
+			const docObj = {
+				...doc.data(),
+				id: doc.id,
+			};
+			setData((data) => [...data, docObj]);
+		});
+	};
+
+	return (
+		<div className='absolute top-[calc(61px)]  min-h-[calc(100vh-61px)] w-screen'>
+			<Carousel />
+			<div className='mb-[calc(61px)] p-5'>
+				{data.map((d) => (
+					<ul className='mt-5' key={d.id}>
+						<li>제목: {d.title}</li>
+						<li>내용: {d.body}</li>
+						<li onClick={() => navigate(`/user/${d.creatorId}`)}>작성자: {d.creatorDisplayName}</li>
+						{d.createdAt && <li>{elapsedTime(d.createdAt)}</li>}
+					</ul>
+				))}
+			</div>
+			{/* <Footer /> */}
+		</div>
+	);
 }
 
 export default Home;
+
+const elapsedTime = (date: number): string => {
+	const start = new Date(date);
+	const end = new Date();
+
+	const seconds = Math.floor((end.getTime() - start.getTime()) / 1000);
+	if (seconds < 60) return '방금 전';
+
+	const minutes = seconds / 60;
+	if (minutes < 60) return `${Math.floor(minutes)}분 전`;
+
+	const hours = minutes / 60;
+	if (hours < 24) return `${Math.floor(hours)}시간 전`;
+
+	const days = hours / 24;
+	if (days < 7) return `${Math.floor(days)}일 전`;
+
+	return `${start.toLocaleDateString()}`;
+};
