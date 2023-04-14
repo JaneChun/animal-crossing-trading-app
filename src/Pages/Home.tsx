8ccca7cf8a3a64b3ba7d05df95320bc3d48f9e07
JaneChun <janechun22@gmail.com>
2023-04-14 20:01:14 +0900

    feat: Home - loading indicator

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -56,44 +56,53 @@ function Home() {
 	};
 
 	return (
-		<div className='custom-container'>
+		<div className='custom-container flex flex-col'>
 			<Carousel />
-			<div className='p-7'>
+			<div className='grow p-7'>
 				<div className='mb-4 flex items-center justify-between'>
 					<div className='text-lg font-bold leading-none text-gray-900 dark:text-white'>거래글</div>
 				</div>
-				<ul className='divide-y divide-gray-200 dark:divide-gray-700'>
-					{data.map(
-						(doc) =>
-							doc.createdAt && (
-								<PostUnit
-									key={doc.id}
-									id={doc.id}
-									type={doc.type}
-									title={doc.title}
-									createdAt={doc.createdAt}
-									creatorDisplayName={doc.creatorDisplayName}
-									creatorId={doc.creatorId}
-									rating={doc.creatorRating}
-									count={doc.creatorCount}
-									comments={doc.comments}
-									done={doc.done}
-								/>
-							)
-					)}
-				</ul>
-				<div className='mt-5 flex justify-center'>
-					{isEnd ? (
-						<div className='mt-5 text-sm text-gray-400'>마지막 페이지입니다.</div>
-					) : (
-						<button
-							onClick={nextPage}
-							className='inline-flex items-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200'
-						>
-							더 보기
-						</button>
-					)}
-				</div>
+
+				{data.length === 0 ? (
+					<div className='flex h-full w-full items-center justify-center'>
+						<div className='animate-pulse px-3 py-1 text-center text-xs font-medium leading-none text-mint'>로딩중...</div>
+					</div>
+				) : (
+					<>
+						<ul className='divide-y divide-gray-200 dark:divide-gray-700'>
+							{data.map(
+								(doc) =>
+									doc.createdAt && (
+										<PostUnit
+											key={doc.id}
+											id={doc.id}
+											type={doc.type}
+											title={doc.title}
+											createdAt={doc.createdAt}
+											creatorDisplayName={doc.creatorDisplayName}
+											creatorId={doc.creatorId}
+											rating={doc.creatorRating}
+											count={doc.creatorCount}
+											comments={doc.comments}
+											done={doc.done}
+										/>
+									)
+							)}
+						</ul>
+						<div className='mt-5 flex justify-center'>
+							{isEnd ? (
+								<div className='mt-5 text-sm text-gray-400'>마지막 페이지입니다.</div>
+							) : (
+								<button
+									onClick={nextPage}
+									className='inline-flex items-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200'
+								>
+									더 보기
+								</button>
+							)}
+						</div>
+					</>
+				)}
 			</div>
 			<Footer />
 		</div>
